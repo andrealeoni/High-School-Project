9008e4c179390d5f4a4b905da4a804e4623c68bb
GiovanniRivolta <35254273+GiovanniRivolta@users.noreply.github.com>
2018-01-13 10:47:20 +0100

    Add files via upload

--- Executor-2.cpp
+++ Executor-2.cpp
@@ -15,12 +15,12 @@ UDP sMask(21102);
 unsigned long ip_address;
 unsigned short port_number;
 char stringend = '\0';
-char str[1000] = { " > C:\\Users\\g.rivolta\\Desktop\\Executor\\Executor\\CMDResults.txt" };
-unsigned char cd[65] = { "cd > C:\\Users\\g.rivolta\\Desktop\\Executor\\Executor\\CMDResults.txt" };
+char str[1000] = { " > C:\\Users\\t.digiorgio\\Desktop\\Executor\\Executor\\CMDResults.txt" };
+unsigned char cd[67] = { "cd > C:\\Users\\t.digiorgio\\Desktop\\Executor\\Executor\\CMDResults.txt" };
 unsigned char n[dimbuffer];
 unsigned char cmdhistory[100000];
 unsigned char tmphistory[100000];
-char execute[58] = { "C:\\Users\\g.rivolta\\Desktop\\Executor\\Executor\\Commands.bat" };
+char execute[60] = { "C:\\Users\\t.digiorgio\\Desktop\\Executor\\Executor\\Commands.bat" };
 char error[50] = { "Comando errato" };
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -51,8 +51,7 @@ void intToString(int num, char str[]) //Merlo e Grandesso
 
 void socket_send(unsigned char str[]) //Rivolta
 {
-	sMask.send(ip_address, port_number, str, strlen((char*)str));
-	sMask.receive(&ip_address, &port_number, (unsigned char*)ack, sizeof(ack));
+	
 }
 
 void conta() //Merlo
@@ -66,9 +65,13 @@ void conta() //Merlo
 		results.getline(strTemp, 1023);
 		contarighe++;
 	} while ((!results.eof()));
+	results.close();
 	intToString(contarighe, (char*)n);
 	sMask.send(ip_address, port_number, n, strlen((char*)n)); //invio del numero di righe
+	cout << "SENT_N" << endl;
+	cout << contarighe << endl;
 	sMask.receive(&ip_address, &port_number, (unsigned char*)ack, sizeof(ack));
+	cout << "ACK_N" << endl;
 }
 
 void fromFile() //Rivolta
@@ -85,14 +88,17 @@ void fromFile() //Rivolta
 	}
 	else
 	{
-		result.open("CMDResults.txt", ios::in);
 		if (result.is_open())
 		{
 			while (!result.eof())
 			{
 				result.getline(strResult, 1023);
-				strResult[strlen(strResult) - 1] = '\0';
-				socket_send((unsigned char*)strResult);
+				//strResult[strlen(strResult) - 1] = '\0';
+				sMask.send(ip_address, port_number, (unsigned char*)strResult, strlen((char*)strResult));
+				cout << "SENT_LINE" << endl;
+				cout << "$" << strResult << "$" << endl;
+				sMask.receive(&ip_address, &port_number, (unsigned char*)ack, sizeof(ack));
+				cout << "ACK_LINE" << endl;
 			}
 		}
 		result.close();
@@ -107,20 +113,22 @@ int main(void)	//Di Giorgio
 	cout << "Servizio attivo..." << "\r\n";
 	while (true)
 	{
-		exe.open("Commands.txt", ios::out);
+		exe.open("Commands.bat", ios::out);
 		Results.open("CMDResults.txt", ios::out);
 		Results << "";
 		Results.close();
 		int j = 0;
 		int k = 0, w = 0;
 		bool flag = false;
-		//sMask.receive(&ip_address, &port_number, n, sizeof(n));
-		//sMask.send(ip_address, port_number, (unsigned char*)ack, sizeof(ack));
-		n[0] = 'c';
+		sMask.receive(&ip_address, &port_number, n, sizeof(n));
+		cout << "RECEIVED" << endl;
+		sMask.send(ip_address, port_number, (unsigned char*)ack, sizeof(ack));
+		cout << "ACK_SENT" << endl;
+		/*n[0] = 'c';
 		n[1] = 'd';
 		n[2] = '.';
 		n[3] = '.';
-		n[4] = '\0';
+		n[4] = '\0';*/
 		White_List.open("WhiteList.txt", ios::in);
 		if (White_List.is_open())
 		{
@@ -163,19 +171,18 @@ int main(void)	//Di Giorgio
 		if (flag)
 		{
 			cout << "Comando corretto" << endl;
-			//Rivolta
 			if (n_commands == 0)
 				strcpy((char*)cmdhistory, (char*)n);
 			else
 				strcat((char*)cmdhistory, (char*)n);
-			strcat((char*)cmdhistory, "\n\r");
 			strcpy((char*)tmphistory, (char*)cmdhistory);
+			//Rivolta
 			if (((n[0] == cd[0]) || (n[0] == (cd[0] - 32))) && ((n[1] == cd[1]) || (n[1] == (cd[1] - 32))))
 			{
 				//strcat((char*)tmphistory, space);
 				exe << tmphistory << "\n\r";
 				exe << cd << "\n\r";
-				exe << error;
+				exe.close();
 				system(execute);
 				cout << "CD" << endl;
 			}
@@ -183,12 +190,12 @@ int main(void)	//Di Giorgio
 			{
 				strcat((char*)tmphistory, str);
 				exe << tmphistory << "\n\r";
+				exe.close();
 				system(execute);
 			}
 			n_commands++;
 			conta();
 			fromFile();
-			exe.close();
 		}
 		else  //Comando non contenuto nella white list
 		{
